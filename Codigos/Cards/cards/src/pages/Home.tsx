f8d7e36e0c2c77dd0f93120c449e99f107243eae
HenriqueDev025 <carlossanttos365@gmail.com>
2026-05-18 16:18:03 -0300

    Aula dia 1805
    Iniciando a aula

--- Codigos/Cards/cards/src/pages/Home.tsx
+++ Codigos/Cards/cards/src/pages/Home.tsx
@@ -1,28 +1,27 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
+
       <IonHeader>
         <IonToolbar>
           <IonTitle>Blank</IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
-          
         </IonHeader>
         <ExploreContainer />
-            <ion-button expand="block" color="primary">
-              Salvar
-            </ion-button>        
       </IonContent>
+
+      <IonCard/>
     </IonPage>
   );
 };
