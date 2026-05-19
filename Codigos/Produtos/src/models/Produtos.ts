9603bf0508fbe2be4777516928587cb673f10280
HenriqueDev025 <carlossanttos365@gmail.com>
2026-05-19 18:43:01 -0300

    Novo projeto Produtos
    Criando a class

---
+++ Codigos/Produtos/src/models/Produtos.ts
@@ -0,0 +1,14 @@
+export class Produto {
+    nome: string;
+    preco: number;
+    estoque: number;
+
+    constructor(nome: string, preco: number){
+        this.nome = nome;
+        this.preco =  preco;
+        this.estoque = 0;
+    }
+    adicionarEstoque(qtd: number) {
+        this.estoque += qtd;
+    }
+}
