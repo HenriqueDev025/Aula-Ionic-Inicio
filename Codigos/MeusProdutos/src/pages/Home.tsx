614293aa17d4cee9652c347d0f7f1967ee502a18
HenriqueDev025 <carlossanttos365@gmail.com>
2026-05-25 18:19:36 -0300

    Aula de Ionic
    Aula de Ionic sobre produtos do dia 2505

--- Codigos/MeusProdutos/src/pages/Home.tsx
+++ Codigos/MeusProdutos/src/pages/Home.tsx
@@ -1,8 +1,21 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonItem, IonList } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonItem, IonList, IonButton} from '@ionic/react';
 import './Home.css';
-import { Produto } from '../Models/MeusProdutos';
+import { Produto } from '../Models/Produtos';
+import React, { useState } from 'react';
 
 const Home: React.FC = () => {
+
+  const [produtos, setProdutos] = useState<Produto[]>([]);
+
+  function adicionar() {
+    {/*instaciar a classe*/}
+    const nova = new Produto("Refrigerante", 12);
+    nova.adicionarEstoque(10);
+
+    setProdutos([...produtos, nova]);
+
+    console.log(produtos);
+  }
 
   return (
     <IonPage>
@@ -21,6 +34,9 @@ const Home: React.FC = () => {
             <IonInput label="Texto" counter={true} maxlength={20}></IonInput>
           </div>
         </IonList>
+        <IonButton onClick={adicionar}>
+          Adicionar Tarefa
+        </IonButton>
       </IonContent>
     </IonPage>
   );
