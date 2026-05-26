33d30d96137cc4b2bc3b404e3d0d05fb50443499
HenriqueDev025 <carlossanttos365@gmail.com>
2026-05-26 18:36:36 -0300

    Aula Dia 2605
    Commit 2 da Aula do dia 2605

---
+++ Codigos/MeusProdutos/src/pages/Cadastro.tsx
@@ -0,0 +1,69 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonItem, IonList, IonButton } from '@ionic/react';
+import './Cadastro.css';
+import { Produto } from '../Models/Produtos';
+import React, { useState,useRef } from 'react';
+
+const Cadastro: React.FC = () => {
+
+  const [produtos, setProdutos] = useState<Produto[]>([]);
+  const nomeRef = useRef<any>(null);
+  const precoRef = useRef<any>(null);
+  const estoqueRef = useRef<any>(null);
+
+  function adicionarProduto() {
+    const nome = nomeRef.current?.value || "";
+    const preco = parseFloat(precoRef.current?.value || "0");
+    const estoque = parseInt(estoqueRef.current?.value || "0");
+
+    if (nome && preco > 0) {
+      const novoProduto = new Produto(nome, preco);
+      novoProduto.adicionarEstoque(estoque);
+
+      setProdutos([...produtos, novoProduto]);
+
+      console.log("Produto adicionado:", novoProduto);
+      console.log("Produtos:", produtos);
+
+
+      if (nomeRef.current) nomeRef.current.value = "";
+      if (precoRef.current) precoRef.current.value = "";
+      if (estoqueRef.current) estoqueRef.current.value = "";
+    }
+  }
+
+    return (
+      <IonPage>
+
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Blank</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        <IonContent fullscreen>
+
+          <IonList className="ListForm">
+
+            <IonItem>
+              <IonInput ref={nomeRef} label="Nome" placeholder="Produtos"></IonInput>
+            </IonItem>
+
+            <div>
+              <IonInput ref={precoRef} label="Preco" counter={true} maxlength={20}></IonInput>
+            </div>
+
+            <div>
+              <IonInput ref={estoqueRef} label="Estoque" counter={true} maxlength={20}></IonInput>
+            </div>
+
+          <IonButton className='ButCadastro' onClick={adicionarProduto}>Cadastrar Produto</IonButton>
+
+          </IonList>
+
+        </IonContent>
+
+      </IonPage>
+    );
+  };
+
+export default Cadastro;
--- Codigos/MeusProdutos/src/pages/Home.tsx
+++ Codigos/MeusProdutos/src/pages/Home.tsx
@@ -1,36 +1,8 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonItem, IonList, IonButton } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList} from '@ionic/react';
 import './Home.css';
-import { Produto } from '../Models/Produtos';
-import React, { useState,useRef } from 'react';
 
 const Home: React.FC = () => {
-
-  const [produtos, setProdutos] = useState<Produto[]>([]);
-  const nomeRef = useRef<any>(null);
-  const precoRef = useRef<any>(null);
-  const estoqueRef = useRef<any>(null);
-
-  function adicionarProduto() {
-    const nome = nomeRef.current?.value || "";
-    const preco = parseFloat(precoRef.current?.value || "0");
-    const estoque = parseInt(estoqueRef.current?.value || "0");
-
-    if (nome && preco > 0) {
-      const novoProduto = new Produto(nome, preco);
-      novoProduto.adicionarEstoque(estoque);
-
-      setProdutos([...produtos, novoProduto]);
-
-      console.log("Produto adicionado:", novoProduto);
-      console.log("Produtos:", produtos);
-
-
-      if (nomeRef.current) nomeRef.current.value = "";
-      if (precoRef.current) precoRef.current.value = "";
-      if (estoqueRef.current) estoqueRef.current.value = "";
-    }
-  }
-
+  
     return (
       <IonPage>
 
@@ -42,23 +14,9 @@ const Home: React.FC = () => {
 
         <IonContent fullscreen>
 
-          <IonList className="ListForm">
-
-            <IonItem>
-              <IonInput ref={nomeRef} label="Nome" placeholder="Produtos"></IonInput>
-            </IonItem>
-
-            <div>
-              <IonInput ref={precoRef} label="Preco" counter={true} maxlength={20}></IonInput>
-            </div>
-
-            <div>
-              <IonInput ref={estoqueRef} label="Estoque" counter={true} maxlength={20}></IonInput>
-            </div>
+          <IonList>
 
           </IonList>
-
-          <IonButton onClick={adicionarProduto}>Cadastrar Produto</IonButton>
 
         </IonContent>
 
