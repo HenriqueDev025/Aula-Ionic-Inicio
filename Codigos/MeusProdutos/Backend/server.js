5952cc3dfe1ae009e6a97318fa8a4cfd9c4d01ba
HenriqueDev025 <carlossanttos365@gmail.com>
2026-06-02 18:16:51 -0300

    Aula de server dia 02062026
    Tentar subir o server

--- Codigos/MeusProdutos/Backend/server.js
+++ Codigos/MeusProdutos/Backend/server.js
@@ -1,35 +1,53 @@
-const fs = request("fs");
-const express = request("express");
+import express from "express";
+
+import fs from "fs";
 
 const app = express();
 app.use(express.json());
 
-const arquivo = "produtos;json"
+const arquivo = "Produtos.json";
+const PORT = 3000;
 
 app.get("/produtos", (req, res) => {
     const dados = fs.readFileSync(arquivo);
-    const prosdutos = JSON.parse(dados);
+    const produtos = JSON.parse(dados);
     res.json(produtos);
 });
 
-app.post("\produtos",(req, res) => {
+app.post("/produtos",(req, res) => {
     const dados = fs.readFileSync(arquivo);
-    const produtos = JSON.parse(dados)
+    const produtos = JSON.parse(dados);
     
     const novoProduto = {
-        id: Date.now()
+        id: Date.now(),
         ...req.body
     };
     produtos.push(novoProduto);
     fs.writeFileSync(arquivo, JSON.stringify(produtos, null,2));
+    res.json(novoProduto);
+});
+
+app.put("/produtos/:id",(req, res) => {
+    const dados = fs.readFileSync(arquivo);
+    let produtos = JSON.parse(dados);
+
+    produtos = produtos.map(p =>
+        p.id == req.params.id ? { ...p, ...req.body } : p
+    );
+    fs.writeFileSync(arquivo,JSON.stringify(produtos,null,2));
+    res.json({ msg: "Atualizado" });
 });
 
 app.delete("/produtos/:id", (req, res) => {
     const dados = fs.readFileSync(arquivo);
     let produtos = JSON.parse(dados);
 
-    produtos = produtos.filter(p=>p.id != req.params.id);
+    produtos = produtos.filter(p => p.id != req.params.id);
 
-    fs.writeFileSync(arquivo, JSON.stringify(produtos, null, 2))
+    fs.writeFileSync(arquivo, JSON.stringify(produtos, null, 2));
     res.json({msg: "Removido"});
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
