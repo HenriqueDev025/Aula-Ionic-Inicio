b025a51120abd3aa834e83ac408a22f97bf40596
HenriqueDev025 <carlossanttos365@gmail.com>
2026-06-15 18:32:01 -0300

    Aula do dia 15062026
    Server Mysql2

---
+++ Codigos/MeusProdutos/Backend/serverMysql.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql2')
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    passoword: '',
+    database: 'controle_produtos'
+});
+
+const express = require('express');
+const app = express()
+
+const port = 3000;
+
+app.get('/produtos', (req, res)=>{
+    db.query('SELECT * FROM produtos', (err, results) => {
+        res.json(results);
+    });
+});
+app.listen(port, () =>{
+    console.log('Servidor rodando na porta ${port}');
+});
